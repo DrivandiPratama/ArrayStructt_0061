0906182936f80edc4aeaa1f4a6c2102b7e9cec49
DrivandiPratama <dripandipratama@gmail.com>
2025-04-20 16:29:05 +0700

    perulangan pada display

--- struct4.cpp
+++ struct4.cpp
@@ -34,3 +34,19 @@ int main() {
         getline(cin, mhs[i].alamat.provinsi);
         cin.ignore(); // Membersihkan newline dari buffer
     }
+
+    cout << endl;
+    cout << "Menampilkan Data" << endl;
+    cout << endl;
+
+    for (int i = 0; i < 2; i++) {
+        cout << "Mahasiswa ke-" << i + 1 << endl;
+        cout << "NIM     = " << mhs[i].nim << endl;
+        cout << "Nama    = " << mhs[i].nama << endl;
+        cout << "Kota    = " << mhs[i].alamat.kota << endl;
+        cout << "Provinsi = " << mhs[i].alamat.provinsi << endl;
+        cout << endl;
+    }
+
+    return 0;
+}
