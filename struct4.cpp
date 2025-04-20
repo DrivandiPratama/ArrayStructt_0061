e2943fa878a16fff4fa1211296531ea5c794f4f2
DrivandiPratama <dripandipratama@gmail.com>
2025-04-20 16:25:46 +0700

    menambahkan data detail alamat dan mahasiswa

---
+++ struct4.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct DetailAlamat {
+    string kota;
+    string provinsi;
+};
+
+struct Mahasiswa {
+    string nama;
+    string nim;
+    DetailAlamat alamat;
+};
